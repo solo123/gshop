97268b4e33399b4234d791e89540801226e6a7e1
jimmy <solo123@21cn.com>
2016-11-02 15:26:36 +0800

    price

--- app/controllers/product_prices_controller.rb
+++ app/controllers/product_prices_controller.rb
@@ -16,14 +16,12 @@ class ProductPricesController < NestedResourcesController
         item.save
       end
     end
-    redirect_to :action => :index 
+    redirect_to :action => :index
   end
   def load_collection
-    @catalogs = Product.select('catalog, count(*) as count').group(:catalog)
-    catalog = params[:cat]
-    catalog ||= 'guitar'
+    @q = Product.search(params[:q])
     pages = 20
-    @collection = Product.where('catalog like ?', "#{catalog}%").order('catalog, full_name').paginate(:page => params[:page], :per_page => pages)
+    @collection = @q.result(distinct: true).paginate(:page => params[:page], :per_page => pages)
   end 
   private
   def product_price_params
